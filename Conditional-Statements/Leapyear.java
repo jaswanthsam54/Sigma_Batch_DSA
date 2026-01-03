b51c54a918a3fb51c9bc4ddae6c274a73a564eda
Jaswanth Saminepally <saminepallyjaswanth@gmail.com>
2026-01-03 23:38:23 +0530

    Add files via upload

---
+++ Conditional-Statements/Leapyear.java
@@ -0,0 +1,25 @@
+// WriteaJavaprogramthattakesayearfromtheuserandprintwhetherthatyear is a leap year or not
+import java.util.Scanner;
+public class Leapyear {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+
+        boolean isLeapYear;
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            isLeapYear = true;
+        } else {
+            isLeapYear = false;
+        }
+
+        if (isLeapYear) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+
+        scanner.close();
+    }
+}
---
+++ Conditional-Statements/temperature.java
@@ -0,0 +1,7 @@
+public class temperature {
+    public static void main(String[] args) {
+        double celsius = 25.0; // Example Celsius temperature
+        double fahrenheit = (celsius * 9/5) + 32;
+        System.out.println(celsius + " degrees Celsius is equal to " + fahrenheit + " degrees Fahrenheit.");
+    }
+}
